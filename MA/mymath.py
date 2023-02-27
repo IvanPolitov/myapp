78da933fcd0e4b5d363282a9e9455d1d5c793dc3
IvanPolitov <ivan.hanter21@mail.ru>
2023-02-27 17:31:13 +0500

    Добавлены функция генерации примеров и провери, соответствующие кнопки, поле ввода

--- MA/mymath.py
+++ MA/mymath.py
@@ -1,2 +1,14 @@
-def plus(a, b):
-    return a + b
+import random
+
+def gen_plus_minus():
+    sign = '+'
+    a = random.randint(0, 100)
+    b = random.randint(-99, 100)
+    if b <= 0:
+        sign = ''
+    text = str(a) + sign + str(b)
+    result = str(a + b)
+    return text, result
+
+if __name__ == '__main__':
+    print(gen_plus_minus())
--- MA/window.py
+++ MA/window.py
@@ -1,6 +1,17 @@
 import mymath
 import tkinter
 
+
+def create_lb2_lb3():
+    lb2['text'], lb3['text'] = mymath.gen_plus_minus()
+
+
+def check_Entry():
+    if txt.get() == lb3['text']:
+        txt.delete(0, tkinter.END)
+        create_lb2_lb3()
+
+
 if __name__ == '__main__':
     main_window = tkinter.Tk()
     main_window.title("Математика")
@@ -9,7 +20,19 @@ if __name__ == '__main__':
     lb1 = tkinter.Label(main_window, text="Пример: ")
     lb1.grid(column=0, row=0)
 
-    lb2 = tkinter.Label(main_window, text="1+1=")
+    btn = tkinter.Button(main_window, text='Генерировать', command=create_lb2_lb3)
+    btn.grid(column=0, row=1)
+
+    btn1 = tkinter.Button(main_window, text='Проверка', command=check_Entry)
+    btn1.grid(column=2, row=1)
+
+    lb2 = tkinter.Label(main_window, text='')
     lb2.grid(column=1, row=0)
+
+    txt = tkinter.Entry(main_window, width=10)
+    txt.grid(column=2, row=0)
+
+    lb3 = tkinter.Label(main_window, text='')
+    lb3.grid(column=3, row=0)
 
     main_window.mainloop()

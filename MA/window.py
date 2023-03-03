a7607d17ae3daa29db22e41b565b150881f12da9
IvanPolitov <ivan.hanter21@mail.ru>
2023-03-03 16:03:08 +0500

    https://younglinux.info/tkinter/tkinter

--- MA/window.py
+++ MA/window.py
@@ -2,16 +2,14 @@ import mymath
 from tkinter import *
 
 
-class UnitPlusMinus:
+class UnitPlusMinus(Frame):
     def __init__(self, parent):
+        super(UnitPlusMinus, self).__init__()
         self.parent = parent
-        self.initUI()
-        self.centerWindow()
-        self.ent = Entry(self, width=20)
-        self.ent.pack()
+        #self.center_window()
+        self.init_ui()
 
-
-    def centerWindow(self):
+    def center_window(self):
         """Размер окна и центрирование"""
         w = 300
         h = 200
@@ -22,13 +20,25 @@ class UnitPlusMinus:
         x = (sw - w) / 2
         y = (sh - h) / 2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    def initUI(self):
+
+    def init_ui(self):
         self.parent.title("Математика")
 
+        f_main = LabelFrame(self.parent, text='Main block')
+        f_system = LabelFrame(self.parent, text='system')
+        f_main.pack(expand=1, fill=BOTH)
+        f_system.pack(expand=1, fill=X)
+
+        example = Label(f_main, text='example')
+        example.pack()
+        button_quit = Button(f_system, text='Закрыть', command=self.quit)
+        button_quit.pack()
+
+
 def main():
-    mainWindow = Tk()
-    app = UnitPlusMinus(mainWindow)
-    mainWindow.mainloop()
+    main_window = Tk()
+    app = UnitPlusMinus(main_window)
+    main_window.mainloop()
 
 
 if __name__ == '__main__':

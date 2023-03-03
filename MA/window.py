85f58b07fad518d33f074bb194c95288a463abb6
IvanPolitov <ivan.hanter21@mail.ru>
2023-03-03 18:02:22 +0500

    Нормальный вид более менее будем отталкиваться от этого

--- MA/window.py
+++ MA/window.py
@@ -1,17 +1,39 @@
 import mymath
 from tkinter import *
+from tkinter import ttk
 
 
 class UnitPlusMinus(Frame):
     def __init__(self, parent):
         super(UnitPlusMinus, self).__init__()
         self.parent = parent
-        #self.center_window()
-        self.init_ui()
+        self.center_window()
+        self.parent.title("Математика")
+
+        f_main = LabelFrame(self.parent, text='Main block')
+        f_system = LabelFrame(self.parent, text='system')
+        f_main.pack(expand=1, fill=BOTH)
+        f_system.pack(anchor=S, expand=0, fill=X)
+
+        self.example = Label(f_main, text='example', font="Arial 20")
+        self.example.pack(side=LEFT, anchor=NW)
+        self.result = 0
+
+        self.user_result = Entry(f_main, font="Arial 20", width=5)
+        self.user_result.pack(side=LEFT, anchor=NW)
+
+        button_answer = Button(f_main, text='Ответить', command=self.check_answer, font="Arial 14")
+        button_answer.pack(side=LEFT, anchor=NW)
+
+        button_quit = Button(f_system, text='Закрыть', command=self.quit)
+        button_quit.pack(side=RIGHT, anchor=E)
+
+        button_generate = Button(f_system, text='Новый пример', command=self.gen)
+        button_generate.pack(side=LEFT, anchor=W)
 
     def center_window(self):
         """Размер окна и центрирование"""
-        w = 300
+        w = 400
         h = 200
 
         sw = self.parent.winfo_screenwidth()
@@ -21,18 +43,13 @@ class UnitPlusMinus(Frame):
         y = (sh - h) / 2
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-    def init_ui(self):
-        self.parent.title("Математика")
+    def gen(self):
+        self.example['text'], self.result = mymath.gen_plus_minus()
 
-        f_main = LabelFrame(self.parent, text='Main block')
-        f_system = LabelFrame(self.parent, text='system')
-        f_main.pack(expand=1, fill=BOTH)
-        f_system.pack(expand=1, fill=X)
-
-        example = Label(f_main, text='example')
-        example.pack()
-        button_quit = Button(f_system, text='Закрыть', command=self.quit)
-        button_quit.pack()
+    def check_answer(self):
+        if self.user_result.get() == self.result:
+            self.gen()
+            self.user_result.delete(0, END)
 
 
 def main():
